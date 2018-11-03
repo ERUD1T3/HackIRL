e89f8d318b331700cb6ef3abb309179c2347661d
jmoukpe <32252266+jmoukpe@users.noreply.github.com>
2018-11-03 13:11:22 -0400

    updated dataset

--- Source Codes/lion_fish_slayer.m
+++ Source Codes/lion_fish_slayer.m
@@ -1,3 +1,5 @@
 % Source code for the lion Fish slayer neural net
-
-LionFishSlayer = alexnet;
+img1 = imread('http://farm1.static.flickr.com/44/148196327_af9a856e8f.jpg')
+imshow(img1)
+LionFishSlayer = alexnet
+prediction = LionFishSlayer.classify('http://farm1.static.flickr.com/44/148196327_af9a856e8f.jpg')
