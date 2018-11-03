ec07c7bace9edf721619f917c751cc049adb9311
jmoukpe <32252266+jmoukpe@users.noreply.github.com>
2018-11-03 13:38:24 -0400

    Rearranging data and source file

--- Source Codes/lion_fish_slayer.m
+++ Source Codes/lion_fish_slayer.m
@@ -1,5 +1,24 @@
-% Source code for the lion Fish slayer neural net
-img1 = imread('http://farm1.static.flickr.com/44/148196327_af9a856e8f.jpg')
-imshow(img1)
-LionFishSlayer = alexnet
-prediction = LionFishSlayer.classify('http://farm1.static.flickr.com/44/148196327_af9a856e8f.jpg')
+
+
+function lion_fish_slayer()
+
+    % Source code for the lion Fish slayer neural net
+    img1 = readfish('Dataset/lionfish/lionfish.jpg')
+    imshow(img1)
+    LionFishSlayer = alexnet;
+
+    %reading image input layer
+    prediction = LionFishSlayer.classify(img1)
+
+end
+
+%Help functions
+function img = readfish(file)
+    img = imread(file);
+    img = imcrop(img, [130 80 426 426]);
+    img = imresize(img, [227 227]);
+%img = repmat(img, [1 1 3])
+end
+
+
+
