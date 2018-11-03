7972a02aa89e17ae2eb784ed2006c1ba906231b9
PascalPolygon <37524802+PascalPolygon@users.noreply.github.com>
2018-11-03 14:41:33 -0400

    Update training_and_analysis.m

--- Source Codes/training_and_analysis.m
+++ Source Codes/training_and_analysis.m
@@ -7,8 +7,8 @@ numSpecies = numel(categories(fishSpecies)) %num of classes
 [trainImgs,testImgs] = splitEachLabel(fishData,0.6, 'randomized'); % 60% of images used for training
 
 %Transfer training alexnet MATLAB CNN for lion fish termination purposes
-lion_fish_slayer_net = alexnet;
-layers = lion_fish_slayer_net.Layers
+LionFishSlayer = alexnet;
+layers = LionFishSlayer.Layers
 
 %modifying fully connected and calssification layers to accomodate data classes
 fc = fullyConnectedLayer(numSpecies) %new fully connected layer with number of scpies for used dataset
@@ -20,13 +20,13 @@ layers(end) = classificationLayer;
 %less agressive training rate
 opts = trainingOptions('sgdm','InitialLearnRate',0.001); 
 
-[lion_fish_slayer_net,info] = trainNetwork(trainImgs, layers, opts); 
+[LionFishSlayer,info] = trainNetwork(trainImgs, layers, opts); 
 
 %plot training loss
 plot(info.TraininLoss) 
 
 %Feed testing set in the NN
-fishPreds = classify(lion_fish_slayer_net,testImgs)
+fishPreds = classify(LionFishSlayer,testImgs)
 
 %Test performance analysis
 fishAcual = testImgs.Labels
