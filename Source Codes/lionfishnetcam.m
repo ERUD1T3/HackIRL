818745463bc4f4f6ec68ca42d7fdd526182910b5
Technocracy <josiasmoukpe@gmail.com>
2018-11-03 22:01:27 -0400

    update on the lionfish detector

--- Source Codes/lionfishnetcam.m
+++ Source Codes/lionfishnetcam.m
@@ -1,11 +1,41 @@
 %%
 clear rpi
-rpi = raspi();
-cam = cameraboard(rpi,'Resolution','640x480');
+
+rpi = raspi('169.254.0.2','pi', 'raspberry');
+myserialdevice = serialdev(rpi,'/dev/ttyAMA0')
+cam = cameraboard(rpi,'Resolution','1980x1980');
 
 %%
-for i = 1:100
-    img = snapshot(cam);
-    image(img);
+for i = 1:1000
+    
+    clear fishPreds
+    clear img
+    clear raw_img
+    
+    raw_img = snapshot(cam);
+    image(raw_img);
     drawnow;
+    
+    img = readfish(raw_img);
+    imshow(img)
+    
+    tic
+        [fishPreds, confidence] = LionFishSlayer.classify(img);
+    toc
+    
+    if fishPreds == 'lionfish'
+        disp('lionfish detected')
+        break
+    else 
+        disp(fishPreds)
+    end
+    
+end
+
+%Help functions
+function img = readfish(raw_img)
+    %img = imread(file);
+    %img = imcrop(img, [130 80 426 426]);
+    img = imresize(raw_img, [227 227]);
+%img = repmat(img, [1 1 3])
 end
